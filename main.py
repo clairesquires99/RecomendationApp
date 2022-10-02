9e9baae7582170146b938053c6ecb3bf876dc2dd
clairesquires99 <clairesquires99@outlook.com>
2022-10-02 13:24:48 +0100

    Minor fixes - login and signup pages working

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 from . import db
 
 main = Blueprint('main', __name__)
 
-@main.route('/')
-def index():
-    return 'Index'
+@main.route('/home')
+def home():
+    return render_template('home.html')
 
 @main.route('/profile')
 def profile():
