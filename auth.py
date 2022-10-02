28cd57604d0959157bd324a0b137a1cd8cbd7755
clairesquires99 <clairesquires99@outlook.com>
2022-10-02 18:45:48 +0100

    Login and Signup functional

--- auth.py
+++ auth.py
@@ -1,13 +1,31 @@
 from flask import Blueprint, render_template, redirect, url_for, request, flash
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user, login_required, logout_user
 from .models import User
 from . import db
 
 auth = Blueprint('auth', __name__)
 
+@auth.route('/')
 @auth.route('/login')
 def login():
     return render_template('login.html')
+
+@auth.route('/login', methods=['POST'])
+def login_post():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    remember = True if request.form.get('remember') else False
+
+    # Check if user exists
+    user = User.query.filter_by(email=email).first()
+
+    if not user or not check_password_hash(user.password, password):
+        flash('Please check your login details and try again.')
+        return redirect(url_for('auth.login'))
+
+    login_user(user, remember=remember)
+    return redirect(url_for('main.home'))
 
 @auth.route('/signup')
 def signup():
@@ -35,5 +53,7 @@ def signup_post():
     return redirect(url_for('auth.login'))
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return 'Logout'
+    logout_user()
+    return redirect(url_for('auth.login'))
--- main.py
+++ main.py
@@ -1,13 +1,22 @@
+from re import template
 from flask import Blueprint, render_template
+from flask_login import login_required
 from . import db
 
-main = Blueprint('main', __name__)
+main = Blueprint('main', __name__, template_folder='templates')
 
 @main.route('/home')
+@login_required
 def home():
     return render_template('home.html')
 
 @main.route('/profile')
+@login_required
 def profile():
     return 'Profile'
+
+@main.route('/books/')
+@login_required
+def books():
+    return render_template('books.html')
 
