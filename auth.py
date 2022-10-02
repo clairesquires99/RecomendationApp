4d075eabb85aaa74bba8f0d44ac8b551b9b52544
clairesquires99 <clairesquires99@outlook.com>
2022-10-02 17:15:48 +0100

    Signup working - correctly adding new user to DB with password

--- auth.py
+++ auth.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for, request
+from werkzeug.security import generate_password_hash, check_password_hash
+from .models import User
 from . import db
 
 auth = Blueprint('auth', __name__)
@@ -10,6 +12,26 @@ def login():
 @auth.route('/signup')
 def signup():
     return render_template('signup.html')
+
+@auth.route('/signup', methods=['POST'])
+def signup_post():
+    firstname = request.form.get('firstname')
+    lastname = request.form.get('lastname')
+    email = request.form.get('email')
+    password = request.form.get('password')
+    
+    # Check whether this email already exists
+    user = User.query.filter_by(email=email).first()
+    if user:
+        return redirect(url_for('auth.signup'))
+    
+    new_user = User(firstname = firstname, lastname = lastname, 
+        email = email, password = generate_password_hash(password, method='sha256'))
+
+    db.session.add(new_user)
+    db.session.commit()
+
+    return redirect(url_for('auth.login'))
 
 @auth.route('/logout')
 def logout():
