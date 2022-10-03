7f3e77f3254e3ad4a4da7992b5d635e999746331
clairesquires99 <clairesquires99@outlook.com>
2022-10-03 19:23:48 +0100

    Add new follower function implemented

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for, flash
 from flask_login import login_required, current_user
 
 from .models import *
@@ -14,7 +14,7 @@ def home():
 @main.route('/profile')
 @login_required
 def profile():
-    return render_template('profile.html')
+    return render_template('profile.html', user=current_user)
 
 @main.route('/books')
 @login_required
@@ -34,5 +34,30 @@ def show_following():
     followers = db.session.query(Follower.user_B_id).filter(Follower.user_A_id == current_user.id).subquery()
     users = User.query.join(followers, User.id == followers.c.user_B_id)
     return render_template('following.html', following=users)
+
+@main.route('/follow_new', methods=['POST'])
+@login_required
+def follow_new():
+    email = request.form.get('email')
+    # Check if email address is registered
+    user_exists = User.query.filter_by(email = email).first()
+    if not user_exists:
+        flash('This email address is not registered.', 'danger')
+        return redirect(url_for('main.show_following'))
+
+    # Check if already following exists
+    followers = db.session.query(Follower.user_B_id).filter(Follower.user_A_id == current_user.id).subquery()
+    user_followers = User.query.join(followers, User.id == followers.c.user_B_id).subquery()
+    user = User.query.filter(user_followers.c.email == email).first()
+        
+    if user:
+        flash('You are already following this person.', 'danger')
+    else:
+        new_following = Follower(user_A_id = current_user.id, user_B_id = user_exists.id)
+        db.session.add(new_following)
+        db.session.commit()
+        flash('New follower successfully added!', 'success')
+
+    return redirect(url_for('main.show_following'))
 
 
