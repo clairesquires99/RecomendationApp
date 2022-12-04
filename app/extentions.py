906a15a8ac040eeaef6d2c78fa0b4022dba038da
clairesquires99 <clairesquires99@outlook.com>
2022-12-04 16:03:02 +0000

    Fixing bug with env vars and Spotipy

--- app/extentions.py
+++ app/extentions.py
@@ -1,13 +1,16 @@
 import spotipy
-import os
+from os import environ, path
+from dotenv import load_dotenv
 from flask_sqlalchemy import SQLAlchemy
 from spotipy.oauth2 import SpotifyClientCredentials
 
 db = SQLAlchemy()
 
 # Spotify setup
-cid = os.environ.get("SPOTIPY_CLIENT_ID")
-csecret = os.environ.get("SPOTIFY_API_KEY")
+basedir = path.abspath(path.dirname(__file__))
+load_dotenv(path.join(basedir, '..', '.env'))
+cid = environ.get("SPOTIPY_CLIENT_ID")
+csecret = environ.get("SPOTIPY_CLIENT_SECRET")
 client_credentials_manager = SpotifyClientCredentials(
     client_id=cid, client_secret=csecret)
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
